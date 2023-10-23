63a722e894df72e5bc3f8fe51654ad5b92c3a753
Minh Hiếu <buihieu11012004@gmail.com>
2023-10-24 03:07:47 +0700

    tabbutton

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,9 @@
+const TabButton = ({ children, onSelect }) => {
+  console.log("TABBUTTON COMPONENT EXECUTING");
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
+};
+export default TabButton;
